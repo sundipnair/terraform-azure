303cc5c302dedba5fe0265aa313fbbe77ab6e745
sundipnair <sundipnair@capita.co.uk>
2018-12-05 11:53:40 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agent_count" {
+    default = 1
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "aztftestaksCluster"
+}
+
+variable cluster_name {
+    default = "aztftestaksCluster"
+}
+
+variable resource_group_name {
+    default = "aztftestRG"
+}
+
+variable location {
+    default = "westeurope"
+}
